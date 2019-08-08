42efa8a7277c9c1c7941db5ac447cfd3b0b8b1ca
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 10:10:28 -0700

    Add files via upload

---
+++ LogEntry.cs
@@ -0,0 +1,48 @@
+﻿using Penguin.Entities;
+using Penguin.Messaging.Logging;
+using Penguin.Persistence.Abstractions.Attributes.Validation;
+using System;
+
+namespace Penguin.Cms.Logging.Entities
+{
+    /// <summary>
+    /// Represents an individual logged message
+    /// </summary>
+    public class LogEntry : AuditableEntity
+    {
+        #region Properties
+
+        /// <summary>
+        /// Log entries are immutable so changes should never be audited
+        /// </summary>
+        public override bool AuditLogChanges => false;
+
+        /// <summary>
+        /// The Type.ToString of the object that requested the logger managing this entry
+        /// </summary>
+        [MaxLength(100)]
+        public string Caller { get; set; }
+
+        /// <summary>
+        /// The level that this message was logged at
+        /// </summary>
+        public LogLevel Level { get; set; }
+
+        /// <summary>
+        /// The string ID of the session for this logging entry
+        /// </summary>
+        public string Session { get; set; }
+
+        /// <summary>
+        /// The time that this session was initialized
+        /// </summary>
+        public DateTime SessionStart { get; set; }
+
+        /// <summary>
+        /// The text value of the body of this log entry
+        /// </summary>
+        public string Value { get; set; }
+
+        #endregion Properties
+    }
+}
